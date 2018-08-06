4d373456a8f9e5f618761ef61910253ded7c0a42
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-08-06 18:26:07 +0300

    node_modules reinstall in docker

--- logview/frontend/src/index.tsx
+++ logview/frontend/src/index.tsx
@@ -1,33 +1,32 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./containers/App/App";
-import reducer from './redux/reducer';
-import { StoreState } from './types/StoreState';
+import reducer from "./redux/reducer";
+import { StoreState } from "./types/StoreState";
 
-if (typeof(Storage) === 'undefined') {
-	ReactDOM.render(
-		<p>
-			Your browser does not support Web Storage API. Please update your browser or use another one.
-		</p>,
-		document.getElementById('root')
-	);
+if (typeof Storage === "undefined") {
+  ReactDOM.render(
+    <p>
+      Your browser does not support Web Storage API. Please update your browser
+      or use another one.
+    </p>,
+    document.getElementById("root")
+  );
 } else {
-	const store = createStore<StoreState, any, {}, {}>(
+  const store = createStore<StoreState, any, {}, {}>(
     reducer,
-		composeWithDevTools(
-			applyMiddleware(thunk),
-		)
-	);
+    composeWithDevTools(applyMiddleware(thunk))
+  );
 
-	ReactDOM.render(
-		<Provider store={store}>
-			<App msg="Hello World" />
-		</Provider>,
-		document.getElementById('root')
-	);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App msg="Hello World" />
+    </Provider>,
+    document.getElementById("root")
+  );
 }
