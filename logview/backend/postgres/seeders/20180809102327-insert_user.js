8944ea184199e0e1cfd76931997979503bc0c6b3
BogusLove <boguscompany@ukr.net>
2018-08-09 15:46:45 +0300

    bb-36 fill seeder data

--- logview/backend/postgres/seeders/20180809102327-insert_user.js
+++ logview/backend/postgres/seeders/20180809102327-insert_user.js
@@ -2,62 +2,63 @@ const userService = require('../../services/userService');
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		const password = await userService.encryptPassword('123');
-		return queryInterface.bulkInsert('Users', [
-			{
-				name: 'Boguslav Barna',
-				email: 'boguslavbarna@gmail.com',
-				password: password
-			},
-			{
-				name: 'Bogdan Koldun',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Dmitriy Beseda',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Ihor Pankiv',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Maksym Kostiuk',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Nataliia Chernomortseva',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Taras Dybyk',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Volodymyr Vorobets',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Yelyzaveta Havrylenko',
-				email: '@gmail.com',
-				password: password
-			},
-			{
-				name: 'Yuliia Kuznietsova',
-				email: '@gmail.com',
-				password: password
-			}
-		]);
+		const data = createData();
+		const users = [];
+		for (let i = 0; i < 10; i++) {
+			users.push(
+				await userService.create({
+					name: data[i].name,
+					password: 123,
+					email: data[i].email
+				})
+			);
+		}
+		return queryInterface.bulkInsert('Users', users);
 	},
 
 	down: (queryInterface, Sequelize) => {
 		return queryInterface.bulkDelete('Users');
 	}
 };
+
+function createData() {
+	const data = [
+		{
+			name: 'Boguslav Barna',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Bogdan Koldun',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Dmitriy Beseda',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Ihor Pankiv',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Maksym Kostiuk',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Nataliia Chernomortseva',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Volodymyr Vorobets',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Yelyzaveta Havrylenko',
+			email: 'boguslavbarna@gmail.com'
+		},
+		{
+			name: 'Yuliia Kuznietsova',
+			email: 'boguslavbarna@gmail.com'
+		}
+	];
+	return data;
+}
