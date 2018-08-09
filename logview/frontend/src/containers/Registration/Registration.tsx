f3509a23fa54ea282d2c2974df6ee288d6d8219c
Liza <lizagavrilenkooo@gmail.com>
2018-08-09 09:34:19 +0300

    Added registration page component and story

---
+++ logview/frontend/src/containers/Registration/Registration.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import RegistrationForm from './RegistrationForm';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { userRegister } from '../../redux/user/actions';
+import { Wrapper } from './RegistrationStyles';
+
+class Registration extends React.Component {
+	handleSumbit = data => {
+		this.props.actions.userRegister(data);
+	};
+	render() {
+		return (
+			<React.Fragment>
+				<RegistrationForm onSubmit={this.handleSubmit} />
+			</React.Fragment>
+		);
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	actions: bindActionCreators({ userRegister }, dispatch)
+});
+
+export default connect(mapDispatchToProps)(Registration);
--- logview/frontend/stories/index.stories.js
+++ logview/frontend/stories/index.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import App from '../src/containers/App/App';
+import Registration from '../src/containers/Registration/Registration';
 
 storiesOf('App', module).add('App', () => <App msg="Hello World" />);
+storiesOf('Registration', module).add('Registration', () => <Registration />);
