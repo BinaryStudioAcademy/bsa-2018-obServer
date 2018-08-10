c57545ee59b9a78055ffa57a0a212ad2efb6b17e
bohdan-koldun <koldunbohdan@gmail.com>
2018-08-10 15:50:53 +0300

    moved reset password routes to api/userRoutes.js

--- logview/backend/routes/api/index.js
+++ logview/backend/routes/api/index.js
@@ -1,6 +1,5 @@
 const user = require('./userRoutes');
 const signin = require('./signinRoutes');
-const recoveryPassword = require('./recoveryPasswordRoutes');
 const logs = require('./logRoutes');
 
 module.exports = {
@@ -8,6 +7,5 @@ module.exports = {
 		app.use('/api/user', user);
 		app.use('/api', signin);
 		app.use('/api/logs', logs);
-		app.use('/api/user', recoveryPassword);
 	}
 };
