c396cc77f7db4268248791c537a3417149db98fd
BogusLove <boguscompany@ukr.net>
2018-08-10 13:52:05 +0300

    bb-11 bug fixes, update user model

--- logview/backend/domains/postgres/models/user.js
+++ logview/backend/domains/postgres/models/user.js
@@ -1,51 +1,63 @@
 const Sequelize = require('sequelize');
 
 const user = (sequilize, DataTypes) => {
-	const User = sequilize.define('User', {
-		id: {
-			type: DataTypes.UUID,
-			primaryKey: true,
-			defaultValue: Sequelize.UUIDV4
-		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-			validate: {
-				isEmail: true
+	const User = sequilize.define(
+		'User',
+		{
+			id: {
+				type: DataTypes.UUID,
+				primaryKey: true,
+				defaultValue: Sequelize.UUIDV4
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+				validate: {
+					isEmail: true
+				}
+			},
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			active: {
+				type: DataTypes.BOOLEAN,
+				defaultValue: false
+			},
+			resetPasswordToken: {
+				type: DataTypes.STRING,
+				allowNull: true
+			},
+			resetPasswordExpires: {
+				type: DataTypes.DATE,
+				allowNull: true
+			},
+			userActivationToken: {
+				type: DataTypes.STRING,
+				allowNull: true
+			},
+			companyId: {
+				type: DataTypes.UUID,
+				allowNull: true
 			}
 		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		active: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false
-		},
-		resetPasswordToken: {
-			type: DataTypes.STRING,
-			allowNull: true
-		},
-		resetPasswordExpires: {
-			type: DataTypes.DATE,
-			allowNull: true
-		},
-		companyId: {
-			type: DataTypes.UUID,
-			allowNull: false,
-			//delete when company model will exist
-			defaultValue: DataTypes.UUIDV4
-		},
-		userActivationToken: {
-			type: DataTypes.STRING,
-			allowNull: true
+		{
+			classMethods: {
+				associate: models => {
+					User.belongsTo(models.Company),
+						{
+							foreignKey: 'companyId',
+							onDelete: 'CASCADE'
+						};
+				}
+			}
 		}
-	});
+	);
 	return User;
 };
 
