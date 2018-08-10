41a26dfb9bd4555d163f690af6faee58df39c205
Volodya <vvorobets@gmail.com>
2018-08-10 15:53:27 +0300

    Deleted comments from companyService

--- logview/backend/services/companyService.js
+++ logview/backend/services/companyService.js
@@ -7,17 +7,6 @@ class CompanyService {
 		this.saltRounds = 8;
 	}
 
-	/** Checks if name contains any of the forbidden symbols [!@#$%^&*]
-	 * @param {*} name
-	 * In Ukraine:
-	 * Найменування юридичної особи не може бути тотожним найменуванню іншої юридичної особи (крім органів місцевого самоврядування).
-	 * При написанні найменування юридичної особи використовуються:
-	 * - Літери алфавіту;
-	 * - Цифри;
-	 * - Розділові знаки та символи: лапки (" ", “ ”, „ “, « », які є тотожними), крапка (.), кома (,), двокрапка (:),
-	 *  дужки /( )/, апостроф ('), дефіс (-), тире (-), коса риска (/), знак оклику (!), знак питання (?),
-	 *  номер (№), плюс (+), знак рівняння (=), зірочка (*), ет комерційна (@).
-	 */
 	validateName(name) {
 		if (!name || name.search(/\S/g) < 0) {
 			throw new Error(`The Company's name should not be empty`);
@@ -30,11 +19,6 @@ class CompanyService {
 		return true;
 	}
 
-	/**
-	 * Creates random unique token for company-database
-	 * @param {*} name
-	 * 	(!) Needed service to check if company already is in db
-	 */
 	generateToken() {
 		return new Promise((resolve, reject) => {
 			crypto.randomBytes(20, (err, buf) => {
