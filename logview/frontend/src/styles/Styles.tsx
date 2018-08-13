6e817321f47bae5bfa31a5231309e9619c3a93d0
Ihor Pankiv <harry.pankiv@gmail.com>
2018-08-13 17:25:54 +0300

    hp-40: Auto stash before merge of "hp-40" and "origin/bb-34"

---
+++ logview/frontend/src/styles/Styles.tsx
@@ -0,0 +1,38 @@
+import { Submit, CheckboxInput } from './ButtonStyles';
+import {
+	Wrapper,
+	Row,
+	Background,
+	BackgroundContainer,
+	Box,
+	RegisterContainer,
+	LogoContainer,
+    RedirectContainer,
+    RegisterBox
+} from './ContainerStyles';
+import { Form } from './FormStyles';
+import { Title, TextLink, RedirectLink, LogoText } from './TextStyles';
+import { LoginBackground, Logo } from './ImageStyles';
+import { Input } from './InputStyles';
+
+export {
+	Submit,
+	Title,
+	TextLink,
+	RedirectLink,
+	LogoText,
+	LoginBackground,
+	Logo,
+	Input,
+	Wrapper,
+	Row,
+	Background,
+	BackgroundContainer,
+	Box,
+	RegisterContainer,
+	Form,
+	RedirectContainer,
+    LogoContainer,
+    CheckboxInput,
+    RegisterBox
+};
