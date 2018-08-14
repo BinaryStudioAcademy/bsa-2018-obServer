3522995827252268a1f5db51234acd3a074e97dd
Volodya <vvorobets@gmail.com>
2018-08-14 13:10:01 +0300

    Add all examples

--- logview/frontend/src/containers/Quickstart/Quickstart.tsx
+++ logview/frontend/src/containers/Quickstart/Quickstart.tsx
@@ -57,6 +57,38 @@ class Quickstart extends React.Component {
 						</code>
 					</pre>
 					<p>
+						<strong>Using observer in your code</strong>
+					</p>
+					<p>In your express.js application add into index.js:</p>
+					<pre>
+						<code>
+							const logconnect = require("observer").logconnect;
+							<br />
+							... <br />
+							app.use(logconnect);
+						</code>
+					</pre>
+					<p>
+						Then create on your server one more app, that contains
+						only index.js:
+					</p>
+					<pre>
+						<code>
+							const logcollect = require("observer").logcollect;
+							<br />
+							logcollect.start("8080", <br />
+							...options, <br />
+							token: "123af12345b1a12345a6aa1234e123456ae1234b"
+							<br />
+							})
+						</code>
+					</pre>
+					<p>
+						Several logconnect apps send data to logcollect (port
+						:8080 in this case) and logcollect sends token (which
+						serves as id) to database.
+					</p>
+					<p>
 						<strong>Upgrading observer</strong>
 					</p>
 					<pre>
