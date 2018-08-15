801644c96f71759b411c1049ddc421a0d44a5a21
taras d <tarass.dubyk@gmail.com>
2018-08-15 20:57:28 +0300

    lifting server cpu/memory up in document model

--- aggregatedstorage/db/models/logMessage.js
+++ aggregatedstorage/db/models/logMessage.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
 const cpuServer = require('./cpuServer');
 const memoryServer = require('./memoryServer');
+const cpuApp = require('./cpuApp');
+const errorLog = require('./errorLog');
+const httpStats = require('./httpStats');
+const memoryApp = require('./memoryApp');
+const socketsStats = require('./socketsStats');
 
 const logMessage = mongoose.Schema({
   companyId: {
     type: String,
     required: true
   },
+  serverData: {
+    cpuServer: [cpuServer],
+    memoryServer: [memoryServer]
+  },
   appsData: [{
     appId: String,
     appName: String,
     logs: { 
-      cpuServer: [cpuServer],
-      memoryServer: [memoryServer]
+      cpuApp: [cpuApp],
+      memoryApp: [memoryApp],
+      httpStats: [httpStats],
+      socketsStats: [socketsStats],
+      errorLog: [errorLog]
     }
   }]   
 });
--- aggregatedstorage/repositories/logRepository.js
+++ aggregatedstorage/repositories/logRepository.js
@@ -1,5 +1,6 @@
 const connection = require('../db/dbConnect');
 const logMessage = require('../db/models/logMessage');
+const logTypes = require('../utils/logTypes');
 
 class LogRepository {
   constructor() {
@@ -7,43 +8,65 @@ class LogRepository {
   }
 
   create(log, callback) {
-    this.model.findOne({ companyId: log.companyToken, 'appsData.appId': log.app.id}) 
-      .then(res => {
-        if (res) {
-          this.model.update(
-            { companyId: log.companyToken, 'appsData.appId': log.app.id },
-            { $push : { [`appsData.$.logs.${log.logType}`] : {...log.data, timestamp: log.timestamp} }},
-            {upsert: true},
-            callback 
-          ); 
-        } else {
-          this.model.findOne({ companyId: log.companyToken })
-            .then(res => {
-              if (res) {
-                res.appsData.push({
-                  appId: log.app.id,
-                  appName: log.app.name,
-                  logs: {
-                    [log.logType]: [{ ...log.data, timestamp: log.timestamp }]
-                  }
-                });
-                res.save(callback);
-              } else {
-                const doc = new this.model({ 
-                  companyId: log.companyToken, 
-                  appsData: [{
+    if (log.logType === logTypes.CPU_SERVER || log.logType === logTypes.MEMORY_SERVER) {
+      this.model.findOne({ companyId: log.companyToken })
+        .then(res => {
+          if (res) {
+            this.model.update(
+              { companyId: log.companyToken },
+              { $push : { [`serverData.${logTypes.name[log.logType]}`] : { ...log.data, timestamp: log.timestamp }}},
+              {upsert: true},
+              callback 
+            );
+          } else {
+            const doc = new this.model({ 
+              companyId: log.companyToken, 
+              serverData: {
+                [logTypes.name[log.logType]]: { ...log.data, timestamp: log.timestamp }
+              } 
+            });
+            doc.save(callback);
+          }
+        });
+    } else {
+      this.model.findOne({ companyId: log.companyToken, 'appsData.appId': log.app.id }) 
+        .then(res => {
+          if (res) {
+            this.model.update(
+              { companyId: log.companyToken, 'appsData.appId': log.app.id },
+              { $push : { [`appsData.$.logs.${logTypes.name[log.logType]}`] : { ...log.data, timestamp: log.timestamp }}},
+              {upsert: true},
+              callback 
+            ); 
+          } else {
+            this.model.findOne({ companyId: log.companyToken })
+              .then(res => {
+                if (res) {
+                  res.appsData.push({
                     appId: log.app.id,
                     appName: log.app.name,
                     logs: {
-                      [log.logType]: [{ ...log.data, timestamp: log.timestamp }]
+                      [logTypes.name[log.logType]]: [{ ...log.data, timestamp: log.timestamp }]
                     }
-                  }] 
-                });
-                doc.save(callback);
-              }
-            })
-        }
-      });
+                  });
+                  res.save(callback);
+                } else {
+                  const doc = new this.model({ 
+                    companyId: log.companyToken, 
+                    appsData: [{
+                      appId: log.app.id,
+                      appName: log.app.name,
+                      logs: {
+                        [logTypes.name[log.logType]]: [{ ...log.data, timestamp: log.timestamp }]
+                      }
+                    }] 
+                  });
+                  doc.save(callback);
+                }
+              })
+          }
+        });
+    }  
   }
 
   getByCompanyId(id, callback) {
