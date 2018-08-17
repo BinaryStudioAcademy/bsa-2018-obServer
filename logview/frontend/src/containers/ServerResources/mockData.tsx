a8daa7981ca49bac156fdb36e49e992317184823
bohdan-koldun <koldunbohdan@gmail.com>
2018-08-18 02:03:38 +0300

    added chart components to memory and cpu

---
+++ logview/frontend/src/containers/ServerResources/mockData.tsx
@@ -0,0 +1,183 @@
+export const coresLoad = [
+	{ timestamp: '2018-08-15T09:49:58.771Z', core1: 96, core2: 53 },
+	{ timestamp: '2018-08-16T09:49:59.771Z', core1: 66, core2: 63 },
+	{ timestamp: '2018-08-18T09:50:01.771Z', core1: 66, core2: 23 },
+	{ timestamp: '2018-08-18T09:50:02.771Z', core1: 36, core2: 87 },
+	{ timestamp: '2018-08-18T09:50:03.771Z', core1: 11, core2: 23 },
+	{ timestamp: '2018-08-19T09:50:04.771Z', core1: 66, core2: 65 },
+	{ timestamp: '2018-08-20T09:50:05.771Z', core1: 66, core2: 43 },
+	{ timestamp: '2018-08-21T09:50:06.771Z', core1: 56, core2: 83 },
+	{ timestamp: '2018-08-23T09:50:07.771Z', core1: 76, core2: 73 },
+	{ timestamp: '2018-08-15T09:50:08.771Z', core1: 96, core2: 53 },
+	{ timestamp: '2018-08-16T09:50:09.771Z', core1: 66, core2: 63 },
+	{ timestamp: '2018-08-17T09:50:10.771Z', core1: 11, core2: 53 },
+	{ timestamp: '2018-08-18T09:50:11.771Z', core1: 66, core2: 23 },
+	{ timestamp: '2018-08-18T09:50:12.771Z', core1: 36, core2: 87 },
+	{ timestamp: '2018-08-18T09:50:13.771Z', core1: 11, core2: 23 },
+	{ timestamp: '2018-08-19T09:50:14.771Z', core1: 66, core2: 65 },
+	{ timestamp: '2018-08-20T09:50:15.771Z', core1: 66, core2: 43 },
+	{ timestamp: '2018-08-21T09:50:16.771Z', core1: 56, core2: 83 },
+	{ timestamp: '2018-08-23T09:50:17.771Z', core1: 76, core2: 73 }
+];
+
+export const memoryPercent = [
+	{
+		freeMemory: 123,
+		usedMemory: 222,
+		timestamp: '2018-08-15T09:49:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 145,
+		timestamp: '2018-08-15T09:50:58.771Z'
+	},
+	{
+		freeMemory: 23,
+		usedMemory: 232,
+		timestamp: '2018-08-15T09:51:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 232,
+		timestamp: '2018-08-15T09:52:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 222,
+		timestamp: '2018-08-15T09:53:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 145,
+		timestamp: '2018-08-15T09:54:58.771Z'
+	},
+	{
+		freeMemory: 23,
+		usedMemory: 232,
+		timestamp: '2018-08-15T09:55:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 232,
+		timestamp: '2018-08-15T09:56:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 222,
+		timestamp: '2018-08-15T09:57:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 145,
+		timestamp: '2018-08-15T09:58:58.771Z'
+	},
+	{
+		freeMemory: 23,
+		usedMemory: 232,
+		timestamp: '2018-08-15T09:59:58.771Z'
+	},
+	{
+		freeMemory: 123,
+		usedMemory: 232,
+		timestamp: '2018-08-15T09:01:58.771Z'
+	}
+];
+
+export const memoryMB = [
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-15T09:49:58.771Z'
+	},
+	{
+		usedMemory: 457,
+		timestamp: '2018-08-16T09:49:58.771Z'
+	},
+	{
+		usedMemory: 23,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 900,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 800,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-15T09:49:58.771Z'
+	},
+	{
+		usedMemory: 457,
+		timestamp: '2018-08-16T09:49:58.771Z'
+	},
+	{
+		usedMemory: 23,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 900,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 800,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-15T09:49:58.771Z'
+	},
+	{
+		usedMemory: 457,
+		timestamp: '2018-08-16T09:49:58.771Z'
+	},
+	{
+		usedMemory: 23,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 900,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 800,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-15T09:49:58.771Z'
+	},
+	{
+		usedMemory: 457,
+		timestamp: '2018-08-16T09:49:58.771Z'
+	},
+	{
+		usedMemory: 23,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 123,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	},
+	{
+		usedMemory: 900,
+		timestamp: '2018-08-17T09:49:58.771Z'
+	},
+	{
+		usedMemory: 800,
+		timestamp: '2018-08-18T09:49:58.771Z'
+	}
+];
