e8c7104680347e902fa0572ab7b4a55258e42e8b
Bohdan Koldun <38987744+bohdan-koldun@users.noreply.github.com>
2018-08-28 17:12:32 +0300

    Update sockets.js

--- logcollect/sockets/sockets.js
+++ logcollect/sockets/sockets.js
@@ -1,5 +1,5 @@
-const ioClient = require("socket.io-client");
-const eventEmitter = require("../events");
+const ioClient = require('socket.io-client');
+const eventEmitter = require('../events');
 const secretToken = process.env.LOGCOLLECT_SECRET_TOKEN;
 const port = process.env.LOGCOLLECT_PORT;
 const logviewURL = `http://localhost:3060`;
@@ -8,12 +8,12 @@ const logcollectURL = `http://localhost:${port}`;
 module.exports = io => {
   const logviewSocket = ioClient.connect(logviewURL);
 
-  io.set("origins", logcollectURL);
-  io.origins("*:*");
+  io.set('origins', logcollectURL);
+  io.origins('*:*');
 
-  logviewSocket.emit("logcollect get settings", secretToken);
+  logviewSocket.emit('logcollect get settings', secretToken);
 
-  logviewSocket.on("logview post settings", settings => {
-    eventEmitter.emit("get new settings", settings);
+  logviewSocket.on('logview post settings', settings => {
+    eventEmitter.emit('get new settings', settings);
   });
 };
