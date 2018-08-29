b66cdff43c2f5a08be36b7737eea3968f4cab133
bohdan-koldun <koldunbohdan@gmail.com>
2018-08-29 15:43:56 +0300

    added mock data

---
+++ logview/frontend/src/containers/HttpStats/mockData.tsx
@@ -0,0 +1,30 @@
+export const coresLoad = [
+	{
+		logType: 'HTTP_STATS',
+		timestamp: '2018-08-23T08:49:11.647Z',
+		data: {
+			route: '/s',
+			method: 'GET',
+			responseTimeMin: 2.475584,
+			responseTimeMax: 4.15517,
+			responseTimeAvg: 10,
+			bodySizeRequest: 270,
+			bodySizeResponse: 1,
+			requestsCount: 4
+		}
+	},
+	{
+		logType: 'HTTP_STATS',
+		timestamp: '2018-08-23T08:49:11.647Z',
+		data: {
+			route: '/s',
+			method: 'GET',
+			responseTimeMin: 2.475584,
+			responseTimeMax: 4.15517,
+			responseTimeAvg: 10,
+			bodySizeRequest: 270,
+			bodySizeResponse: 1,
+			requestsCount: 4
+		}
+	}
+];
