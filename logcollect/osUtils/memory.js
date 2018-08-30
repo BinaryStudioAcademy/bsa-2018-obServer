31b26acd3680c9bac05c1c47a9d6e26d4892694e
taras d <tarass.dubyk@gmail.com>
2018-08-30 16:43:02 +0300

    fix sys language check

--- logcollect/osUtils/memory.js
+++ logcollect/osUtils/memory.js
@@ -2,7 +2,7 @@ const os = require("os");
 const free = require("free-memory");
 
 module.exports = callback => {
-  if (process.platform === "linux" && process.env.LANG === "en_US.UTF-8") {
+  if (process.platform === "linux" && process.env.LANGUAGE === "en_US") {
     free((err, info) => {
       const freeMemory = Math.floor((info.mem.total - info.mem.used) / 1024);
       const allMemory = Math.floor(info.mem.total / 1024);
