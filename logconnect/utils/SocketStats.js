dad33e63522bedf872705c82029bd657917eb0c1
BogusLove <boguscompany@ukr.net>
2018-08-31 17:08:44 +0300

    bb-106 fixes

--- logconnect/utils/SocketStats.js
+++ logconnect/utils/SocketStats.js
@@ -3,12 +3,10 @@ const monitor = require('socket.io-monitor');
 
 class SocketStats {
     constructor(io, sendLog, appId) {
-        this.io = io;
         this.sendLog = sendLog;
         this.appId = appId;
         this.timers = {};
         this.emitter = monitor.bind(this.io).emitter;
-        this.time = 0;
     }
 
     async calcRoomsAmount(callback) { 
@@ -26,30 +24,20 @@ class SocketStats {
         callback({ socketStats });
     }
 
-    middleware() {
-        this.io.use((socket, next) => {
-            console.log('IN 1 MIDDLEWARE');
-            // for (let key in socket) {
-            //     console.log(key);
-            // }
-            next();
-        });
-    }
-
     startSocketMonitor(delay = 1000) {
-        if (!this.timers.memoryTimedId) {
-            this.timers.memoryTimedId = setInterval(() => {
-                this.calcRoomsAmount(data => {
-                    console.log(data.socketStats.rooms);
+        if (!this.timers.socketsTimerID) {
+            this.timers.socketsTimerID = setInterval(() => {
+                this.calcRoomsAmount(socketsData => {           
+                    const log = createLogObject('SOCKET_APP', socketsData, this.appId);
+                    this.sendLog(log);
                 });
             }, delay);
         }
-        this.middleware();
     }
 
     stopStopMonitor() {
-        clearInterval(this.timers.memoryTimedId);
-        delete this.timers.memoryTimedId;
+        clearInterval(this.timers.socketsTimerID);
+        delete this.timers.socketsTimerID;
     }
 }
 
