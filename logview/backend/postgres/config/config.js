f774f3f2cabceb81445d993fb8cd755d823084c4
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-08-31 14:33:04 +0300

    config sequelize changes

--- logview/backend/postgres/config/config.js
+++ logview/backend/postgres/config/config.js
@@ -15,17 +15,29 @@ module.exports = {
 		seederStoragePath: 'sequelize_data.json'
 	},
 	test: {
-		username: 'database_test',
-		password: 'database_test',
-		database: 'database_test',
-		host: 'localhost',
-		dialect: 'postgres'
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PWD,
+		database: process.env.POSTGRES_DB,
+		host: process.env.HOST,
+		dialect: process.env.DB_DIALECT,
+
+		migrationStorage: 'json',
+		migrationStoragePath: 'sequelize_meta.json',
+
+		seederStorage: 'json',
+		seederStoragePath: 'sequelize_data.json'
 	},
 	production: {
-		username: 'database_prod',
-		password: 'database_prod',
-		database: 'database_prod',
-		host: 'localhost',
-		dialect: 'postgres'
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PWD,
+		database: process.env.POSTGRES_DB,
+		host: process.env.HOST,
+		dialect: process.env.DB_DIALECT,
+
+		migrationStorage: 'json',
+		migrationStoragePath: 'sequelize_meta.json',
+
+		seederStorage: 'json',
+		seederStoragePath: 'sequelize_data.json'
 	}
 };
