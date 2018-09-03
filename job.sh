617bfe82ef19ebf469c97c3a22321e770eaad3b8
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-03 12:08:57 +0300

    travis changes

--- job.sh
+++ job.sh
@@ -4,13 +4,15 @@ echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 if [ "$TRAVIS_BRANCH" == "logview" ]
 then
     export PROJECT_NAME=logview
-elif [ "$TRAVIS_BRANCH" == "raw-storage" ]
+elif [ "$TRAVIS_BRANCH" == "raw-store" ]
 then
     export PROJECT_NAME=rawstorage
-elif [ "$TRAVIS_BRANCH" == "aggregated-storage" ]
+elif [ "$TRAVIS_BRANCH" == "aggregated-store" ]
 then
     export PROJECT_NAME=aggregatedstorage
 fi
+
+echo "$PROJECT_NAME: $PROJECT_NAME"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $PROJECT_NAME ./$PROJECT_NAME
