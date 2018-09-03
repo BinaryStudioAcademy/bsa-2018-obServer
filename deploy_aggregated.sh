3256435dd97be49413fbc48041655ecb84f3ec55
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-03 09:18:22 +0300

    add deploy files for raw and aggreagted store projects

---
+++ deploy_aggregated.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PROJECT_NAME=aggregated-storage
+export DOCKER_IMAGE_NAME=dmitriybeseda/$PROJECT_NAME
+export INSTANCE=$PROJECT_NAME
+
+set -e
+
+echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker stop $PROJECT_NAME && docker rm $PROJECT_NAME && docker rmi -f $DOCKER_IMAGE_NAME && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3060 --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
