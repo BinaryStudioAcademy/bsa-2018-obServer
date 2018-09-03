0291a02c097fd1c020c2a1a8435a032629836b82
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-03 10:25:31 +0300

    travis changes

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DOCKER_ACCOUNT_NAME=dmitriybeseda
+if [$TRAVIS_BRANCH=logview]
+then
+    export PROJECT_NAME=logview
+elif [$TRAVIS_BRANCH=raw-storage]
+then
+    export PROJECT_NAME=raw-storage
+elif [$TRAVIS_BRANCH=aggregated-storage]
+then
+    export PROJECT_NAME=aggregated-storage
+fi
+    
+
+export DOCKER_IMAGE_NAME=$DOCKER_ACCOUNT_NAME/$PROJECT_NAME
+export INSTANCE=$PROJECT_NAME
+
+set -e
+
+echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker stop $PROJECT_NAME && docker rm $PROJECT_NAME && docker rmi -f $DOCKER_IMAGE_NAME && docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3060 --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
---
+++ job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [$TRAVIS_BRANCH=logview]
+then
+    export PROJECT_NAME=logview
+elif [$TRAVIS_BRANCH=raw-storage]
+then
+    export PROJECT_NAME=rawstorage
+elif [$TRAVIS_BRANCH=aggregated-storage]
+then
+    export PROJECT_NAME=aggregatedstorage
+fi
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t $PROJECT_NAME ./$PROJECT_NAME
+docker tag $PROJECT_NAME $DOCKER_USERNAME/$PROJECT_NAME
+docker push $DOCKER_USERNAME/$PROJECT_NAME
