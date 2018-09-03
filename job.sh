2f0cd6c2df610f96108d3ab986ecfce3ac98b211
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-03 12:08:35 +0300

    test prjocect name

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $TRAVIS_BRANCH
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 if [ "$TRAVIS_BRANCH" == "logview" ]
 then
     export PROJECT_NAME=logview
@@ -14,5 +14,5 @@ fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $PROJECT_NAME ./$PROJECT_NAME
-docker tag $PROJECT_NAME $DOCKER_USERNAME/$PROJECT_NAME
+docker tag "$PROJECT_NAME $DOCKER_USERNAME/$PROJECT_NAME"
 docker push $DOCKER_USERNAME/$PROJECT_NAME
