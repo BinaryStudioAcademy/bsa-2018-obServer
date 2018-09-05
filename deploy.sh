fd2277f1782001288bce974f76ebb9a39c00c1f4
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-05 13:03:35 +0300

    without env

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,7 @@ then
     gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "\
     docker pull mongo && docker run -it -d -p 27017:27017 \
     docker pull rabbitmq && docker run -it -d -p 5672:5672 \
-    docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3055 --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
+    docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3055 --name=$PROJECT_NAME $DOCKER_IMAGE_NAME"
 elif [ "$TRAVIS_BRANCH" == "aggregated-store" ]
 then
     gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3055 --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
