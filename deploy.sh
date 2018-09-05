d532dc62e40ae0147cb073d5a176fdd7e6d59e4a
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-05 09:46:48 +0300

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -23,9 +23,9 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 # gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "(docker kill $PROJECT_NAME || true) && docker rm $PROJECT_NAME && docker rmi -f $DOCKER_IMAGE_NAME"
 gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker run docker/compose:$DOCKER_COMPOSE_V version"
-gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:/rootfs/$PWD" \
-    -w="/rootfs/$PWD" \
-    docker/compose:$DOCKER_COMPOSE_V up --build -d"
+# gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker run --rm \
+#     -v /var/run/docker.sock:/var/run/docker.sock \
+#     -v "$PWD:/rootfs/$PWD" \
+#     -w="/rootfs/$PWD" \
+#     docker/compose:$DOCKER_COMPOSE_V up --build -d"
 gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3055 --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
