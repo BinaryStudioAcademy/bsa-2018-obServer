839ce98b64f9c37ff87dd2cb85db0322d6f99301
Dmitriy Beseda <dmitriy.beseda@binary-studio.com>
2018-09-06 17:20:23 +0300

    added travis ci

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "DEPLOY ITSELF HAS BEEN STARTED"
+
+if [ "$TRAVIS_BRANCH" == "logview" ]
+then
+    export PROJECT_NAME=logview
+elif [ "$TRAVIS_BRANCH" == "raw-store" ]
+then
+    export PROJECT_NAME=rawstorage
+elif [ "$TRAVIS_BRANCH" == "aggregated-store" ]
+then
+    export PROJECT_NAME=aggregated-storage
+fi
+
+export DOCKER_ACCOUNT_NAME=dmitriybeseda
+export DOCKER_IMAGE_NAME=$DOCKER_ACCOUNT_NAME/$PROJECT_NAME
+export INSTANCE=$PROJECT_NAME
+export DOCKER_COMPOSE_V=1.22.0
+
+set -e
+
+echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+if [ "$TRAVIS_BRANCH" == "logview" ]
+then
+    gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "\ 
+    docker stop logview && docker rm logview && docker rmi -f $DOCKER_IMAGE_NAME && \
+    docker pull $DOCKER_IMAGE_NAME && \
+    docker run -it -d -p 80:3060 -v /home/$USER/sequelize:/sequelize --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
+elif [ "$TRAVIS_BRANCH" == "raw-store" ]
+then
+    ls
+    gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "\
+    docker pull mongo && docker run -it -d -p 27017:27017 \
+    docker pull rabbitmq && docker run -it -d -p 5672:5672 \
+    docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3055 --name=$PROJECT_NAME $DOCKER_IMAGE_NAME"
+elif [ "$TRAVIS_BRANCH" == "aggregated-store" ]
+then
+    gcloud compute --project $PROJECT ssh --zone $ZONE $INSTANCE --command "docker pull $DOCKER_IMAGE_NAME && docker run -it -d -p 80:3055 --name=$PROJECT_NAME --env-file '1.env' $DOCKER_IMAGE_NAME"
+fi
---
+++ job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+if [ "$TRAVIS_BRANCH" == "logview" ]
+then
+    export PROJECT_NAME=logview
+elif [ "$TRAVIS_BRANCH" == "raw-store" ]
+then
+    export PROJECT_NAME=rawstorage
+elif [ "$TRAVIS_BRANCH" == "aggregated-store" ]
+then
+    export PROJECT_NAME=aggregatedstorage
+fi
+
+echo "PROJECT_NAME: $PROJECT_NAME"
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t $PROJECT_NAME ./$PROJECT_NAME
+docker tag $PROJECT_NAME $DOCKER_USERNAME/$PROJECT_NAME
+docker push $DOCKER_USERNAME/$PROJECT_NAME
