a07770a7e59d0fda413493c820ab65490ef4fbe5
BogusLove <boguscompany@ukr.net>
2018-09-11 17:19:16 +0300

    bb-106 new lib for cpu

--- logconnect/utils/CPUStats.js
+++ logconnect/utils/CPUStats.js
@@ -1,5 +1,5 @@
 const createLogObject = require('./createLogObject');
-const usage = require('usage');
+const pidusage = require('pidusage');
 
 class CPUAppStats {
     constructor(sendLog, appId) {
@@ -9,9 +9,9 @@ class CPUAppStats {
     }
 
     calcCPU(callback) {
-        usage.lookup(process.pid, (err, data) => {
+        pidusage(process.pid, (err, data) => {
             if (err) console.error(err);
-            else {
+            else {                              
                 const cpuUsagePercentages  = Math.round(data.cpu * 100) / 100;
                 callback({ cpuUsagePercentages });
             }
